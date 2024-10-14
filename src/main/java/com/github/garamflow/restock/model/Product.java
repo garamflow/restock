5f49af9da22d9c455c375cc2e68c9265274a82a1
garamflow <garamflow@gmail.com>
2024-10-14 18:14:11 +0900

    feat: getter 및 재고 감소 메서드와 재고 품절 상태 체크 메서드 추가

--- src/main/java/com/github/garamflow/restock/model/Product.java
+++ src/main/java/com/github/garamflow/restock/model/Product.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,7 +26,22 @@ public class Product {
         this.reStockRound = 0; // 초기 재입고 회차는 0으로 설정
         this.createdAt = LocalDateTime.now();
     }
+
+    protected Product() {}
+
     public void incrementReStockRound() {
         this.reStockRound++;
+    }
+
+    // 재고 감소 메서드 (재고가 부족하면 예외 발생)
+    public void decreaseStock(int quantity) {
+        if (this.stock - quantity < 0) {
+            throw new IllegalStateException("재고가 부족합니다.");
+        }
+        this.stock -= quantity;
+    }
+
+    public boolean isOutOfStock() {
+        return this.stock <= 0;
     }
 }
