45c3fd9c33bd1ca28619eb48ba547529cfcd8627
garamflow <garamflow@gmail.com>
2024-10-14 18:51:15 +0900

    refactor: transactional 추가

--- src/main/java/com/github/garamflow/restock/service/ProductNotificationServiceImpl.java
+++ src/main/java/com/github/garamflow/restock/service/ProductNotificationServiceImpl.java
@@ -7,6 +7,7 @@ import com.github.garamflow.restock.repository.ProductUserNotificationRepository
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -15,6 +16,7 @@ import java.util.NoSuchElementException;
 public class ProductNotificationServiceImpl implements ProductNotificationService {
 
     private static final Logger log = LoggerFactory.getLogger(ProductNotificationServiceImpl.class);
+
     private final ProductNotificationHistoryRepository productNotificationHistoryRepository;
     private final ProductUserNotificationRepository productUserNotificationRepository;
     private final ProductRepository productRepository;
@@ -25,7 +27,7 @@ public class ProductNotificationServiceImpl implements ProductNotificationServic
         this.productRepository = productRepository;
     }
 
-
+    @Transactional
     @Override
     public void sendReStockNotification(Long productId) {
 
@@ -38,6 +40,7 @@ public class ProductNotificationServiceImpl implements ProductNotificationServic
 
         // 알림 설정 유저 목록 조회
         List<ProductUserNotification> userNotifications = productUserNotificationRepository.findByProductId(productId);
+
 
         // 전송 상태 변경
         NotificationStatus status = NotificationStatus.IN_PROGRESS;
@@ -67,6 +70,7 @@ public class ProductNotificationServiceImpl implements ProductNotificationServic
         saveProductNotificationHistory(product, status, lastNotifiedUserId);
     }
 
+    @Transactional
     @Override
     public List<ProductNotificationHistory> getNotificationHistory(Long productId) {
         try {
@@ -78,7 +82,6 @@ public class ProductNotificationServiceImpl implements ProductNotificationServic
 
     private void sendNotification(Long productId, Long userId) {
         // 실제 알림 발송 로직
-//        log.info("상품 ID: {}에 대한 재입고 알림을 사용자 ID: {}에게 발송했습니다.", productId, userId);
     }
 
     private void saveProductNotificationHistory(Product product, NotificationStatus status, Long lastNotifiedUserId) {
